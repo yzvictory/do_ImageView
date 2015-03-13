508468e2f098a38b2b9f85e1bf1a599858fb7f09
zhouyi <643988878@qq.com>
2015-03-13 17:00:26 +0800

    image

---
+++ src/windows/do_ImageView/ui/do_ImageView/extdefine/do_ImageView_MAbstract.cs
@@ -0,0 +1,26 @@
+﻿using doCore.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_ImageView.extdefine
+{
+    public abstract class do_ImageView_MAbstract : doUIModule
+    {
+        protected do_ImageView_MAbstract()
+            : base()
+        {
+            
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public override void OnInit()
+        {
+            base.OnInit();
+            //注册属性
+        }
+    }
+}

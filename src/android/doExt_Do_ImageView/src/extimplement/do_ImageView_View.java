eec95e709720d10d5741191c87760405df77293e
zhuozuoying <zhuozuoying@163.com>
2015-03-30 11:06:31 +0800

    update

--- src/android/doExt_Do_ImageView/src/extimplement/do_ImageView_View.java
+++ src/android/doExt_Do_ImageView/src/extimplement/do_ImageView_View.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import core.DoServiceContainer;
 import core.helper.DoIOHelper;
 import core.helper.DoImageHandleHelper;
 import core.helper.DoImageLoadHelper;
@@ -208,7 +209,7 @@ public class do_ImageView_View extends ImageView implements DoIUIModuleView, do_
 					}
 				}
 			} catch (Exception e) {
-				throw new RuntimeException(e);
+				DoServiceContainer.getLogEngine().writeError("do_ImageView_View", e);
 			}
 		}
 	}
